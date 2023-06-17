b66cdc8630e044b0cb2d2b213d17df69e0807494
Kiara Jeff <ki.mij@icloud.com>
2023-06-17 13:15:25 +0000

    updated photos index and show

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  #mount_uploader :image, ImageUploader
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
   has_secure_password
